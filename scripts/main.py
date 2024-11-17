f0e732aaaecf0e94b1de251d9c54e3029d50f333
retvq <ritvik2403@gmail.com>
2024-11-17 22:39:02 +0530

    PreProcessed Data Successfully

--- scripts/main.py
+++ scripts/main.py
@@ -1,2 +1,1 @@
-path = r'C:\Users\DSUCSCL7-22\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts'
-print([ord(char) for char in path])
+print("Welcome to Healthcare Scheme Recommendation System")
